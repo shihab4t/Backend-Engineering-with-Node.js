19f321ad36dcef6510230cb613995633db2188ec
Shihab Mahamud <67628903+shihab4t@users.noreply.github.com>
2022-05-23 03:50:07 +0600

    Event

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ const EventEmitter = require("node:events");
 // click, double click, hover are event
 // listen => define a function against a event
 // emit => trigger a event
+
+const myEmitter = new EventEmitter();
+// console.log(myEmitter);
+
+// register
+myEmitter.on("event", () => {
+    console.log("an event has triggered");
+});
+
+myEmitter.on("event", () => {
+    console.log("triggered again");
+});
+
+// trigger
+myEmitter.emit("event");
